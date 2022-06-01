32023b91cf4a90b7e2ae6d254aa49b40dc9d3ed8
neeoll <arias.noel24@gmail.com>
2022-06-01 12:39:57 -0400

    Configured deployment for Goerli testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,26 +1,17 @@
-require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-waffle")
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const ALCHEMY_API_KEY = "MCMFr3BnL4IqMABkqhIqFFGParjITlPF"
+const GOERLI_PRIVATE_KEY = "a89792116b214273ba5c64bb57b0b6844a43fe86c5ac75e10dd7d29ebcfe69bc"
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.9",
   networks: {
     hardhat: {
       chainId: 31337
     },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
   },
 };
